51c31870fe9c38816d0d6087911a72bc14bd47e1
Kangheemyung <kang.2025kensyu@gmail.com>
2025-08-22 17:25:53 +0900

    add

--- src/main/java/jp/co/sss/crud/controller/RegistrationController.java
+++ src/main/java/jp/co/sss/crud/controller/RegistrationController.java
@@ -1,10 +1,17 @@
 package jp.co.sss.crud.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import jp.co.sss.crud.bean.EmployeeBean;
 
 @Controller
 public class RegistrationController {
 
-  
-    
+
+    @GetMapping("regist/regist_input")
+    public String returnToRegistrationForm(@ModelAttribute("employee") EmployeeBean employee) {
+        return "regist/regist_input";
+    }
 }
